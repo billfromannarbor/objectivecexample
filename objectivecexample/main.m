23a5dfeefd47f3e966d0690f8bfe2c92f7a93720
Bill Heitzeg <billheitzeg@othermac>
2016-10-25 21:13:43 -0400

    Initial Commit

---
+++ objectivecexample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objectivecexample
+//
+//  Created by Bill Heitzeg on 10/25/16.
+//  Copyright © 2016 Bill Heitzeg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
